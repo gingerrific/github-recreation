e22c52efc7a56cfcd5ebe0bbb05ef92f46ac991f
Josh Date <date0010@me.com>
2014-05-15 20:04:55 -0400

    added moment.js to fix time issues on repo divs. also added more styling to sidebar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,5 @@
 "use strict";
 
-
-// var numStarred = 0;
-// // does a loop through the repo data in search of any 
-// // starred repos and adds them to an existing variable
-// repoInfo.forEach(function(repo) {
-//   numStarred = numStarred + repo.stargazers_count;
-// });
-// // adds the value to your user data object, if it's not there it creates it
-// githubUser[0]["starred"] = numStarred;
-
 ///// Templates/////////////////////////////////////
 ////////////////////////////////////////////////////
 var sideBarTemplate = _.template($('.side-bar-content-template').text());
@@ -20,24 +10,27 @@ var repoTemplate = _.template($('.repo-template').text());
 ////////////////////////////////////////////////////
 
 function userRendering (user){
+      user.created_at = moment(user.created_at).format("MMMM Do, YYYY");
       var gingerNinja = sideBarTemplate(user);
       $('.side-bar').prepend(gingerNinja);
 }
 
 function repoRendering (data) {
-  data.forEach(function (repo){
-    var repository = repoTemplate(repo);
-    $('.repo-list').append(repository);
+    _.sortBy(data, function (sortedData) {
+      return sortedData.updated_at; //sorts the repos by their updated at value. default is ascending order
+    }).reverse().forEach(function (repo){ // this reverses it and then calls the for each to further parse the data
+      repo.updated_at = (moment(repo.updated_at).fromNow()); //this formats the updated_at filed into a period of time from the current moment.
+      var repository = repoTemplate(repo);
+      $('.repo-list').append(repository);
   });
 }
 
+function starReporting (starCount) {
+  $('.star-counter').append(starCount.length);
+}
 
-// repoInfo.forEach(function (repo){
-//  var repository = repoTemplate(repo);
-//  $('.repo-list').append(repository);
-// });
 
-///// Repos ////////////////////////////////////////
+///// API Calls ////////////////////////////////////
 ////////////////////////////////////////////////////
 
 $.getJSON('https://api.github.com/users/gingerrific' + apiKEY).done(function (user){
@@ -47,6 +40,11 @@ $.getJSON('https://api.github.com/users/gingerrific' + apiKEY).done(function (us
 $.getJSON('https://api.github.com/users/gingerrific/repos' + apiKEY).done(function (repos){
   repoRendering(repos);
 });
+
+$.getJSON('https://api.github.com/users/gingerrific/starred' + apiKEY).done(function (stars){
+  starReporting(stars);
+});
+
 
 
 
